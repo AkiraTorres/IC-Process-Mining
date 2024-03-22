f181988eebc5d8dd1895f99fe3f7c21b274736b9
AkiraTorres <torres.thg@gmail.com>
2024-03-22 17:36:20 -0300

    fix: adjusted test_mining.py to work automatically with the 8 scenaries

--- test_mining.py
+++ test_mining.py
@@ -1,6 +1,17 @@
 from gsppy.gsp import GSP
 import json, sys
 
+sceneries = [
+    "1-first",
+    "2-second",
+    "3-third",
+    "4-fourth",
+    "5-fifth",
+    "6-sixth",
+    "7-seventh",
+    "8-eighth",
+]
+
 
 def gsp_mining(sequences: list) -> list:
     result = GSP(sequences).search(0.08)
@@ -14,23 +25,53 @@ def format_tf_data(sequences: list) -> list:
     return formated
 
 
-def read_params():
-    json_data = "./teste.json"
+def read_params(file: str) -> str:
+    file_name = f"./sceneries/{file}.json"
     if len(sys.argv) > 1:
-        json_data = sys.argv[1]
-    return json_data
+        file_name = sys.argv[1]
+    return file_name
 
 
-def main():
-    json_data = read_params()
+def write_result(result: list, file: str) -> None:
+    with open(f"./sceneries_result/{file}.json", "w+") as _file:
+        json.dump(result, _file)
 
-    with open(json_data, "r") as file:
-        all_sequences = json.load(file)
 
-    formated = format_tf_data(all_sequences)
+def remap_keys(unit: dict) -> dict:
+    seq_quantities = len(list(unit)[0])
+    result = {}
+    result[f"{seq_quantities}_sequences"] = [
+        {"sequence": key, "total": value} for key, value in unit.items()
+    ]
+    return result
 
-    for i in gsp_mining(formated):
-        print(i, end="\n\n")
+
+def remap_keys_old(unit: dict) -> list:
+    return [{"sequence": key, "total": value} for key, value in unit.items()]
+
+
+def main() -> None:
+    for scenerie in sceneries:
+        file_name = read_params(scenerie)
+
+        with open(file_name, "r") as file:
+            all_sequences = json.load(file)
+
+        formated = format_tf_data(all_sequences)
+
+        mining_result = gsp_mining(formated)
+        result = []
+
+        for unit in mining_result:
+            result.append(remap_keys(unit))
+            # print("\n\n")
+
+        # mining_result = [dict(sequence=str(i[0]), support=i[1]) for i in mining_result]
+
+        write_result(result, scenerie)
+        # for i in result:
+        #     print(i)
+        #     print("\n\n")
 
 
 if __name__ == "__main__":
